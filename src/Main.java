a6034d430edf7a5c8c30eb572356353803188c34
Mohaned Ashraf <businessmohaned@gmail.com>
2025-05-08 06:03:55 +0300

    multi-threading

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
 import repository.DatabaseRepository;
 import service.BusinessLogic;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class Main {
     private static final DatabaseRepository databaseRepository = new DatabaseRepository();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         long timeStarted = System.currentTimeMillis();
 
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        List<Future<Void>> futures = new ArrayList<>();
 
         List<String> sentences = databaseRepository.findDataFromDatabase();
         for (String sentence : sentences) {
-            BusinessLogic.fibSentence(sentence);
+            futures.add(executor.submit(() -> BusinessLogic.fibSentence(sentence)));
+
         }
 
+        for (Future<Void> future : futures) {
+            future.get();
+        }
 
+        executor.shutdown();
 
         long timeEnded = System.currentTimeMillis();
         long timeTakenToExecute = (timeEnded - timeStarted);
--- src/service/BusinessLogic.java
+++ src/service/BusinessLogic.java
@@ -3,11 +3,12 @@ package service;
 public class BusinessLogic {
     private static final CpuIntensiveBusinessService cpuIntensiveBusinessService = new CpuIntensiveBusinessService();
 
-    public static void fibSentence(String sentence) {
+    public static Void fibSentence(String sentence) {
         int length = sentence.length();
         long fibonacciPerSentence = cpuIntensiveBusinessService.fibonacci(length);
         String logMessage = String.format("String: %s | Length: %d | Fibonacci: %d | Thread: %s",
                 sentence, length, fibonacciPerSentence, Thread.currentThread().getName());
         System.out.println(logMessage);
+        return null;
     }
 }
